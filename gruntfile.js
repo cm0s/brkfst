81d3e8a55c25a4c2646c93bf0da4afb03c8b8758
Nicolas Forney <nicolas@eforney.com>
2014-01-01 22:31:22 +0100

    chore(gruntjs): add server.js dependency in the grunt-mocha-cli

--- gruntfile.js
+++ gruntfile.js
@@ -79,7 +79,7 @@ module.exports = function (grunt) {
         },
         mochacli: {
             options: {
-                require: ['should'],
+                require: ['should','server.js'],
                 reporter: ['spec'],
                 bail: true, //Stop on the first exception
                 files: ['test/mocha/**/*.js']
