5718067a798291c9f83a96480133801e4cbd4879
Nicolas Forney <nicolas@eforney.com>
2014-01-13 22:56:35 +0100

    chore(sample): update sample data with some appCategories

--- test/mongodb/sample-data.js
+++ test/mongodb/sample-data.js
@@ -1,11 +1,34 @@
 db.apps.remove();
+db.appCategories.remove();
 
+
+/**
+ * AppCategory
+ */
+var appCategory1Id = ObjectId(),
+  appCategory2Id = ObjectId();
+
+db.appCategories.insert({
+    _id: appCategory1Id,
+    title: 'Nice and fun'
+  },
+  {
+    _id: appCategory2Id,
+    title: 'Real stuff'
+  });
+
+/**
+ * App
+ */
 db.apps.insert({
   'title': 'My Rocket App',
   'url': 'http://www.google.com',
   icon: {
     url: 'img/icons/rocket-icon.svg'
-  }
+  },
+  categories:[
+    appCategory1Id,appCategory2Id
+  ]
 });
 
 db.apps.insert({
@@ -13,7 +36,10 @@ db.apps.insert({
   'url': 'http://www.google.com',
   icon: {
     url: 'img/icons/phone-icon.svg'
-  }
+  },
+  categories:[
+    appCategory2Id
+  ]
 });
 
 db.apps.insert({
@@ -21,7 +47,10 @@ db.apps.insert({
   'url': 'http://www.google.com',
   icon: {
     url: 'img/icons/headphones-icon.svg'
-  }
+  },
+  categories:[
+    appCategory1Id
+  ]
 });
 
 db.apps.insert({
@@ -29,5 +58,8 @@ db.apps.insert({
   'url': 'http://www.google.com',
   icon: {
     url: 'img/icons/calendar-icon.svg'
-  }
+  },
+  categories:[
+    appCategory2Id
+  ]
 });
