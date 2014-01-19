b9d9242725681d47394f932c5d28d45f5d94b19a
Nicolas Forney <nicolas@eforney.com>
2014-01-19 18:18:40 +0100

    refactor(angular:catalog): rename variable

--- public/angular/app/catalog/catalog.js
+++ public/angular/app/catalog/catalog.js
@@ -1,7 +1,6 @@
 angular.module('catalogCtrl', ['services.apiRestangularSrv'])
   .controller('CatalogCtrl', function ($scope, apiRestangularSrv) {
-    var apps = apiRestangularSrv.all('apps');
-    $scope.apps = apps.getList().$object;
-
+    var appCategories = apiRestangularSrv.all('appCategories');
+    $scope.appCategories = appCategories.getList().$object;
   });
 
