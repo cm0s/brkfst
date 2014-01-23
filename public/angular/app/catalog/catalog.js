d14394453e88f2ad2e37d3c3c487ae4d4f0bad8c
Nicolas Forney <nicolas@eforney.com>
2014-01-23 01:18:58 +0100

    feat(catalog): add search filter to ignore diacritical marks and thus be able to type normal char and return results containing diacritics

--- public/angular/app/catalog/catalog.js
+++ public/angular/app/catalog/catalog.js
@@ -1,6 +1,17 @@
-angular.module('catalogCtrl', ['services.apiRestangularSrv','ui.utils'])
-  .controller('CatalogCtrl', function ($scope, apiRestangularSrv) {
+angular.module('catalogCtrl', ['services.apiRestangularSrv', 'services.utilsSrv', 'ui.utils'])
+  .controller('CatalogCtrl', function ($scope, apiRestangularSrv, utilsSrv) {
     var appCategories = apiRestangularSrv.all('appCategories');
     $scope.appCategories = appCategories.getList().$object;
+
+    $scope.ignoreAccents = function (item) {
+      if (!$scope.search) {
+        console.log('true');
+        return true;
+      }
+      var itemTitle = utilsSrv.replaceDiacritics(item.title).toLowerCase();
+      var searchInput = utilsSrv.replaceDiacritics($scope.search).toLowerCase();
+      console.log('searchInput'+searchInput+'  itemTitle:'+itemTitle);
+      return itemTitle.indexOf(searchInput) > -1;
+    };
   });
 
