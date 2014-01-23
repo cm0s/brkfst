29b4687b24f28a31f05c5398e9f2ccd4170b6fa0
Nicolas Forney <nicolas@eforney.com>
2014-01-23 01:20:05 +0100

    fix(catalog): opss just forget to remove console.log

--- public/angular/app/catalog/catalog.js
+++ public/angular/app/catalog/catalog.js
@@ -5,12 +5,10 @@ angular.module('catalogCtrl', ['services.apiRestangularSrv', 'services.utilsSrv'
 
     $scope.ignoreAccents = function (item) {
       if (!$scope.search) {
-        console.log('true');
         return true;
       }
       var itemTitle = utilsSrv.replaceDiacritics(item.title).toLowerCase();
       var searchInput = utilsSrv.replaceDiacritics($scope.search).toLowerCase();
-      console.log('searchInput'+searchInput+'  itemTitle:'+itemTitle);
       return itemTitle.indexOf(searchInput) > -1;
     };
   });
