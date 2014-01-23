be9a2cb2e0d063f14a786296c0a7a8124c496200
Nicolas Forney <nicolas@eforney.com>
2014-01-23 23:27:57 +0100

    feat(catalog): enhance the search input filter
    It's now possible to enter multiple value separated by n numbers of special chars.
    
    Actual list : ,.$*+()_- and space char

--- public/angular/app/catalog/catalog.js
+++ public/angular/app/catalog/catalog.js
@@ -4,18 +4,37 @@ angular.module('catalogCtrl', ['services.apiRestangularSrv', 'services.utilsSrv'
     $scope.appCategories = appCategories.getList().$object;
     $scope.isSearchInputEmpty = true;
 
-    $scope.hideCategories = function(){
+    $scope.hideCategories = function () {
       $scope.isSearchInputEmpty = _.isEmpty($scope.search);
     };
 
-    //Filter used to ignore accents when filtering apps
-    $scope.ignoreAccents = function (item) {
+    //Filter which ignores accents and also handle text separated by special chars like space, dot, etc.
+    $scope.textFilter = function (item) {
       if (!$scope.search) {
         return true;
       }
+
+      //Replace all special char and lowercase the result
       var itemTitle = utilsSrv.replaceDiacritics(item.title).toLowerCase();
       var searchInput = utilsSrv.replaceDiacritics($scope.search).toLowerCase();
-      return itemTitle.indexOf(searchInput) > -1;
+
+      //Split search input with the following char: , .$*+()_-
+      var searchInputSplit = searchInput.split(/(?:,| |\.|\$|\*|\+|\(|\)|_|-)+/);
+
+      //Run a lodash find function to see if one of the split element of
+      //the search input is not found. If at least one is not found it means the
+      //filter must indicates it's a no match (return false). Otherwise it match
+      //and the filter return true.
+      var elementNotFound =_.find(searchInputSplit, function(searchInputElement){
+        if (itemTitle.indexOf(searchInputElement) < 0) {
+          return true;
+        }
+        return false;
+      });
+      if(!elementNotFound){
+        return true;
+      }
+      return false;
     };
   });
 
