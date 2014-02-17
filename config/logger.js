26e68c4e2ea5dda795f5ecadac7a959b288580de
Nicolas Forney <nicolas@eforney.com>
2014-02-17 23:10:31 +0100

    feat(logger): add a logger with console logging
    Use winston logging library

---
+++ config/logger.js
@@ -0,0 +1,14 @@
+/**
+ * Logger module using flatiron/winston logging library
+ */
+var winston = require('winston');
+
+var logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)()
+  ]
+});
+
+module.exports = logger;
+
+
