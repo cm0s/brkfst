a78d32fa65ed5442f9a5e325145747d2f4bbe148
Nicolas Forney <nicolas@eforney.com>
2014-03-01 14:18:04 +0100

    refactor(mysql): replace keys.map loop by lodash forEach

--- config/mysql.js
+++ config/mysql.js
@@ -1,6 +1,7 @@
 var mysql = require('mysql'),
   config = require('./config'),
-  conn = mysql.createConnection(config.db);
+  conn = mysql.createConnection(config.db),
+  _ = require('lodash');
 
 exports.conn = conn;
 
@@ -21,11 +22,18 @@ conn._insert = function (table, fields, callback) {
 };
 
 conn._upsert = function (table, fields, callback) {
-  if (!fields.id) return conn._insert(table, fields, callback);
-  var keys = Object.keys(fields);
-  var values = keys.map(function (k) {
-    return fields[k];
+  if (!fields.id) {
+    return conn._insert(table, fields, callback);
+  }
+
+  var values = [],
+    keys = [];
+
+  _.forEach(fields, function (value, key) {
+    values.push(value);
+    keys.push(key);
   });
+
   var querystring
     = 'UPDATE `' + table + '` SET ' +
     keys.map(function (k) {
